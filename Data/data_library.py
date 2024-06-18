e28005b71ca16ef660ee09a37b90e2f8e74e5e2a
TVernet <112699327+TVernet@users.noreply.github.com>
2024-06-18 10:06:03 +0200

    Update data_library.py

--- Data/data_library.py
+++ Data/data_library.py
@@ -1,3 +1,3 @@
-# Dynamic user data dictionary
+# Dynamic dictionary of user data entered in "data_management.py". 
 expense = {"loyer": 14500, "courses": 8700, "internet": 3000}
 income = {"salaire": 24000}
